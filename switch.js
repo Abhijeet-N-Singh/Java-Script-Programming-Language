c9bf0c21c3e7cb3975d01f79aeaf9806c6c9017a
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-05 16:30:27 +0530

    switch.js
    this is n repository for JavaScript denoting switch method.

---
+++ switch.js
@@ -0,0 +1,28 @@
+let day=5;
+
+switch(day)
+{
+    case 0:
+        console.log("MOnday");
+        break;
+    case 1:
+        console.log("Tuesday");
+        break;
+    case 2:
+        console.log("Wednesday");
+    case 3:
+        console.log("Thrusday");
+        break;
+    case 4:
+        console.log("Friday");
+        break;
+    case 5:
+        console.log("Saturday");
+        break;
+    case 6:
+        console.log("Sunday");
+        break;
+    
+    default:
+        console.log("Invalid Day");
+}
