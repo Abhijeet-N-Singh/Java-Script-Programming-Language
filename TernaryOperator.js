a207c89def02e9259556f0e35b61c5283e5df9f7
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-06 12:31:38 +0530

    TernaryOperator.js
    nested if else function used !

---
+++ TernaryOperator.js
@@ -0,0 +1,14 @@
+let age=4;
+//let drink;
+//if (age>=5)
+//{
+//    drink="coffee";
+//}
+//else
+//{
+//    drink="MIlk";
+//}
+//console.log(drink); or 
+
+let drink = age>=5 ? "coffee" : "MILk";
+console.log(drink);
