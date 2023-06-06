c0544d7825bdead2b621e76026a02b4bb2b6ba7e
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-06 12:32:39 +0530

    Variable.js
    //variable is a reserved memory location used for storing the values 
    
    //var x=10;
    //var y=20;

---
+++ Variable.js
@@ -0,0 +1,12 @@
+//variable is a reserved memory location used for 
+//storing the values 
+
+//var x=10;
+//var y=20;
+let x=10;
+let y=22;
+let z=30;
+console.log(x);
+console.log(y);
+console.log(typeof(x));
+console.log(typeof(y));
