a73c864842c3ad93311dd8d12a8c5bf2a145f63a
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-06 12:22:23 +0530

    Exception.js

---
+++ Exception.js
@@ -0,0 +1,13 @@
+function divide(a,b)
+{
+    if(b==0)
+    {
+        throw 'Division by zero';
+    }
+    return a/b;
+}
+
+let result=divide(10,5);
+console.log(result);
+//data types 
+// numbers , strings
