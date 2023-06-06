f883917d23ab83d75f90a30469bd764acccf5fcf
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-06 12:24:16 +0530

    Function.js
    Function is a group of statements written to perform some specific tasks +, -, %,* etc

---
+++ Function.js
@@ -0,0 +1,10 @@
+//Function is a group of statements written to 
+//perform some specific tasks +, -, %,?
+
+function add(a,b)//==> parameter are nothing but variables
+{
+    return a+b;
+}
+
+let sum=add(2,3);//==> Arguments or values assigned to the parameters
+console.log(sum);
