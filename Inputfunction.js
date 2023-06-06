7923f709433100d7065df70cb11e3d29a47d2c79
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-06 12:28:03 +0530

    Inputfunction.js
    let userGuess = +prompt("Guess a Number");
    // 
    
    nested if statement

---
+++ Inputfunction.js
@@ -0,0 +1,22 @@
+let winnignumber=19;
+
+//let userGuess = +prompt("GUess a Number");
+// nested if statement
+
+let userGuess = 20
+if(userGuess === winnignumber)
+{
+    console.log("Your GUess is Correct");
+}
+else
+{
+    if(userGuess < winnignumber)
+    {
+        console.log("YOur guess is too low for the winning number")
+    }
+    else
+    {
+        console.log("Your Guess is too High for the winning number")
+    }
+}
+ i 
