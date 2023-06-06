507ce4dfd19b01694bae35efd9ad73abf21aa527
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-06 12:33:54 +0530

    morevariables.js
    let digit=12;
    let decimal=2.56;
    let letter="Hello";
    let bool=true;

---
+++ variables.js
@@ -0,0 +1,8 @@
+let digit=12;
+let decimal=2.56;
+let letter="Hello";
+let bool=true;
+console.log(typeof(digit));
+console.log(typeof(decimal));
+console.log(typeof(letter));
+console.log(typeof(bool));
