5d00f7fc780065dd359d9cfc248881f1e0bca3f2
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-09 15:37:51 +0530

    BooleansComparision.js
    This is a repository for JavaScript program showing comparision operators in boolean data types !

---
+++ BooleansComparision.js
@@ -0,0 +1,20 @@
+// Booleans and comparision operators
+// booleans datatypes have two values
+// True or False   
+
+let num1 = 7;
+let num2 = "7";
+
+console.log(num1<num2);
+console.log(num1<=num2);
+console.log(num1>num2);
+console.log(num1>=num2);
+// == vs ==
+console.log(num1 == num2);
+console.log(num1 === num2);
+// = is used fro assign the values
+// == is used for comparing the values between two variables.assigning
+// === is used for comparision of two different datatypes
+// != vs !==
+console.log(num1 != num2);
+console.log(num1 !== num2);
