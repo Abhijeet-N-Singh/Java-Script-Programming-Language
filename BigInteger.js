5f9955e57ebb57fa014f65096ff1cd841644c6e5
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-09 15:21:22 +0530

    BigInteger.js

---
+++ BigInteger.js
@@ -0,0 +1,6 @@
+let myNumber = 123n;
+console.log(typeof myNumber)
+let sameMyNumber = BigInt(123);
+console.log(typeof sameMyNumber);
+console.log(myNumber);
+console.log(myNumber+ sameMyNumber);  
