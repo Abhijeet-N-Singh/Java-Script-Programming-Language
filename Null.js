5e703d5faecb0e0a61cbad8fe7945316a8b8d7e2
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-09 14:50:15 +0530

    Null.js
    this is a repository for JavaScript program depicting Null string as well .

---
+++ Null.js
@@ -0,0 +1,18 @@
+// undefined
+// null
+// let age = 22;
+
+let firstName;
+firstName="Abhijeet Singh";
+console.log(typeof firstName,firstName);
+
+//console.log(typeof firstName);
+//firstName = "deep";
+
+let myVariable = null
+console.log(myVariable);
+myVariable = "deep";
+console.log(myVariable, typeof myVariable);
+console.log(typeof null);
+
+// bug , error
