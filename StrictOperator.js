1f2e9fabfbf90e624a12ff620be0324e12f59793
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-09 15:51:03 +0530

    StrictOperator.js
    This is an repository for depicting the Strict Operator Datatypes program in JavaScript .

---
+++ StrictOperator.js
@@ -0,0 +1,13 @@
+
+let num = 13;
+if(num%2===0)
+// === is also called as strict equality operator.
+// first it compares the datatype then it compares the value.
+
+{
+    console.log("even");
+}
+else
+{
+    console.log("odd");
+}
