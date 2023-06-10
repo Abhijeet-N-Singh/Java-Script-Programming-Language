2f38d1935b50f62c46ffc8971b9f6957763c6d40
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-10 19:48:25 +0530

    Iterativestatement2.js

---
+++ Iterativestatement2.js
@@ -0,0 +1,8 @@
+// for loop
+// the for loop is used to loop through or iterate over a sequence.
+let total = 0;
+let num = 100;
+for (let i = 1; i<=num; i++){
+    total = total + i;
+}
+console.log("The total of 100 number is:", total);
