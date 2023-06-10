dc52c0d66b3998536cb80173b9ebde0268aeec08
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-10 17:18:37 +0530

    identityoperator.js

---
+++ identityoperator.js
@@ -0,0 +1,9 @@
+let fruits = ["apple", "mango","papaya"];
+let obj = {}; // object literal
+console.log(fruits);
+fruits[1] = "banana";
+console.log()(fruits);
+console.log(typeof fruits);
+console.log(typeof obj);
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(obj));
