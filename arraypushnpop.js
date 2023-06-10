03e3642d8417be52d20b49388d6af79d90aa7ca3
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-10 17:45:00 +0530

    arraypushnpop.js
    this is an repository for array in JavaScript showing push pop and shift unshift methods of adding and removing or changing the item in the array .

---
+++ arraypushnpop.js
@@ -0,0 +1,17 @@
+ let fruits = ["apple", "watermelon","papaya"];
+ console.log(fruits);
+ // push = adding the item to the array
+ fruits.push("banana");
+ fruits[1]="orange"// changing the item in the array
+ console.log(fruits);
+ // pop =
+ let poppedFruit = fruits.pop();//it is going to remove the last item of the array
+ console.log(fruits);
+ console.log("popped fruits is", poppedFruit);
+ // unshift = adds an element to the beginning of the array
+ fruits.unshift("banana");
+ console.log(fruits);
+ fruits.shift()
+ console.log(fruits)
+ fruits.unshift("myfruit");
+ console.log(fruits);
