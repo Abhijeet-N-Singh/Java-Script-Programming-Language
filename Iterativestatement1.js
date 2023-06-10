8501bd27e4d8475de2d96be87d357ab46e4e2df6
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-10 19:46:50 +0530

    Iterativestatement1.js

---
+++ Iterativestatement1.js
@@ -0,0 +1,15 @@
+// IN JavaScript itrative means repetative . 
+// 3 types of loops are there
+// --> the while loop
+// --> the for loop
+// --> the do while loop
+
+//while loop
+let i=0; 
+while (i<=9) //0<=9, 1<=9, 2<=9.....10<=9
+{
+    console.log(`current value of i is ${i}`);
+    i++;
+}
+console.log("Iterations of 'while loop' is ended!!!");
+console.log(`current value of i after while loop ended its iteration's is ${i}`);
