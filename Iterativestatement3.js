d46474f1154614a5e7e918bac53a50f7c461bdd7
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-10 19:50:52 +0530

    Iterativestatement3.js

---
+++ Iterativestatement3.js
@@ -0,0 +1,16 @@
+// the do while loop
+// its a variant of the whhile loop.
+// This loop will execute the code block once, before checking
+// if the condition is true, then it will repeat the loop as long 
+// as the condition is true.
+// waArning = if yo dont increment the counter, the while loop will not stop untill forever !
+ let i = 0;
+ do
+ {
+    console.log(`current value of i is ${i}`);
+    i++;
+ }
+ while(i<=9);
+
+ console.log("Iterations of 'do while' loop is ended!!!")
+ console.log(`current value of i after iteration is ${i}`);
