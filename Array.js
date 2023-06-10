adc0f7a5dd7982222ef92cf4ba026e04a613bfb2
Abhijeet Singh <93851757+Abhiyayo@users.noreply.github.com>
2023-06-10 17:04:53 +0530

    Array.js

---
+++ Array.js
@@ -0,0 +1,16 @@
+// In JavaScript it is called as array
+// in python it is called as list
+// how to create arrays
+
+// list is an ordered collection of items which is placed inside
+// those square brackets are seperated by commas(,). A list may
+// contain mixed data types in array.
+
+let fruits = ["apple", "mango", "watermelon"];
+let numbers = [1,2,3,4];
+let mixed = [1,2,2.3, "string", null, undefined];
+console.log(mixed);
+console.log(numbers);
+console.log(fruits[2]);
+let age;// data type called undefined.
+console.log(typeof age);
